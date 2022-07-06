cd3aa5dfc77242085d8ad8d5c7c7f70d8131d69b
MJB <matt.berti13@gmail.com>
2022-07-06 18:22:58 -0500

    added mongo to server

--- server/server.js
+++ server/server.js
@@ -39,6 +39,8 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
 }
 
