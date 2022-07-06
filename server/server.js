cb32b5a190fcc6f44db5903812703fd5780686d3
MJB <matt.berti13@gmail.com>
2022-07-06 18:20:33 -0500

    refactored server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,34 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
+const { ApolloServer } = require('apollo-server-express');
+
+const { authMiddleware } = require('./utils/auth');
+
+
+
+
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
+const startApolloServer = async(typeDefs, resolvers) =>{
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -19,3 +40,8 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+}
+
+
+
+
